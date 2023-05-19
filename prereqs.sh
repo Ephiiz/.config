8ad0c2f423891c72900d4f2aa0b1ca7b1520854a
Taylor <36703779+Ephiiz@users.noreply.github.com>
2023-05-19 13:09:33 -0400

    Update prereqs.sh

--- prereqs.sh
+++ prereqs.sh
@@ -2,3 +2,15 @@
 
 # Ensure rustup is installed for Rust development
 curl https://sh.rustup.rs -sSf | sh 
+
+# Install Useful software
+cargo install bat
+cargo install 
+#Nushell
+cargo install nu
+
+
+# Install Helix editor
+sudo add-apt-repository ppa:maveonair/helix-editor
+sudo apt update
+sudo apt install helix
